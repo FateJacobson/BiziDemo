e38559ef8fe35fac256ed73b1754b8c58de71715
Fate Jacobson <82694104+FateJacobson@users.noreply.github.com>
2022-03-09 22:37:55 -0800

    Update register.page.ts

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-register',
@@ -8,13 +10,28 @@ import { ModalController } from '@ionic/angular';
 })
 export class RegisterPage implements OnInit {
 
+  name: ''
+  email: ''
+  password: ''
   constructor(
+    private http: HttpClient,
     public modalCtrl: ModalController,
   ) { }
 
   ngOnInit() {
   }
 
+  register() {
+    let post_url = 'http://brysonreese.duckdns.org:5000/api/v1/users/'
+
+      this.http.post(post_url, {
+        name: this.name,
+        email: this.email,
+        password: this.password
+      }).toPromise().then((data: any) => {
+        console.log(data.token)
+      })
+  }
   async dismiss() {
     return await this.modalCtrl.dismiss();
   }
