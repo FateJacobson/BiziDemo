d20551bbf13e0798fb02642922a8634f275aa2ba
Fate Jacobson <82694104+FateJacobson@users.noreply.github.com>
2022-03-09 22:35:58 -0800

    Update login.page.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +9,26 @@ import { ModalController } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
 
+  email: ''
+  password: ''
   constructor(
+    private http: HttpClient,
     public modalCtrl: ModalController,
   ) { }
 
   ngOnInit() {
   }
 
+  login() {
+    let post_url = 'http://brysonreese.duckdns.org:5000/api/v1/users/authenticate'
+
+      this.http.post(post_url, {
+        email: this.email,
+        password: this.password
+      }).toPromise().then((data: any) => {
+        console.log(data.token)
+      })
+  }
   async dismiss() {
     await this.modalCtrl.dismiss();
   }
